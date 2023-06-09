b7887d4af6b6e93e214ebfea0381029a6d45774f
escotoj <j.escoto14@gmail.com>
2023-06-08 20:05:05 -0700

    added data to user.js based on structure specified

--- models/User.js
+++ models/User.js
@@ -1,8 +1,43 @@
-const { Schema, Types } = require('mongoose')
+const { Schema, Types, model } = require('mongoose')
+// model funtion is imported frm mong which creates models for our db
 
 const userSchema = new Schema(
-    // DATA
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            trimmed: true,
+            max_length: 25,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            match: [/.+@.+\..+/],
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        }
+    ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }    
 );
+
+// virtual property
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
 
 const User = model('user', userSchema)
 
