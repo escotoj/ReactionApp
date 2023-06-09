5df0f4b9e4273f693ce9097f1580aad3aaa8504f
escotoj <j.escoto14@gmail.com>
2023-06-08 18:08:24 -0700

    got the server running after fixing error

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
+require('dotenv').config()
 
+const express = require('express');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next()
+})
+
+
 app.listen(PORT, () => {
-    console.log('listeing....')
+    console.log('listeing.... on http://localhost:3001/')
 });
